104d101af6c2be8abfc915f0fcba2f8665cc6d0d
Jatin Kumar <jk066634@gmail.com>
2023-03-13 08:54:17 +0530

    added guass_elimination_with_parial_pivoting

---
+++ Guass_Elimination_partial_pivoting.m
@@ -0,0 +1,28 @@
+A = input('Enter your coeff matrix ');
+B = input('Enter source matrix ');
+N = length(B);
+X = zeros(N,1);
+Aug = [A B];
+for j = 1:N-1
+    %partial pivoting
+    [M,P] = max(abs(Aug(j:N,j)))
+    C = Aug(j,:);
+    Aug(j,:) = Aug(P+j-1,:);
+    Aug(P+j-1) = C;
+
+    %echelon form
+    for i = j+1:N
+        m=Aug(i,j)/Aug(j,j);
+        Aug(i,:)=Aug(i,:) - m*Aug(j,:);
+    end
+end
+
+
+Aug
+
+X(N) = Aug(N,N+1)/Aug(N,N);
+for k = N-1:-1:1
+    X(k) = Aug(k,N+1) - Aug(k,k+1:N)*X(k+1:N)/Aug(k,k);
+end
+
+X
