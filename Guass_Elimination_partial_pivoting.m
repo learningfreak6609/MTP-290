95322151df784f2f43e2821ade42eb3cd8a4cd4f
Jatin Kumar <jk066634@gmail.com>
2023-03-13 10:25:21 +0530

    Update Guass_Elimination_partial_pivoting.m

--- Guass_Elimination_partial_pivoting.m
+++ Guass_Elimination_partial_pivoting.m
@@ -1,3 +1,4 @@
+
 A = input('Enter your coeff matrix ');
 B = input('Enter source matrix ');
 N = length(B);
@@ -5,10 +6,10 @@ X = zeros(N,1);
 Aug = [A B];
 for j = 1:N-1
     %partial pivoting
-    [M,P] = max(abs(Aug(j:N,j)))
+    [M,P] = max(abs(Aug(j:N,j)));
     C = Aug(j,:);
     Aug(j,:) = Aug(P+j-1,:);
-    Aug(P+j-1) = C;
+    Aug(P+j-1,:) = C;
 
     %echelon form
     for i = j+1:N
@@ -22,7 +23,10 @@ Aug
 
 X(N) = Aug(N,N+1)/Aug(N,N);
 for k = N-1:-1:1
-    X(k) = Aug(k,N+1) - Aug(k,k+1:N)*X(k+1:N)/Aug(k,k);
+
+    X(k) = (Aug(k,N+1) - Aug(k,k+1:N)*X(k+1:N))/Aug(k,k);
+    %disp(X(k));
 end
 
 X
+
